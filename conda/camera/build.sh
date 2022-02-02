2bc7b5d419ed4fe25bd348370fcf4afaa4ba8e55
Samuel Debionne <samuel.debionne@esrf.fr>
2022-02-02 11:45:32 +0100

    Revert "[CONDA] Add post-built simlinks creation for the SDK libraries"
    This reverts commit 0ee7e1d033fc28224f0d2ae0056b83e08c485c22.

--- conda/camera/build.sh
+++ conda/camera/build.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cmake -Bbuild -H. -G "Ninja" -DLIMA_ENABLE_PYTHON=1 -DCAMERA_ENABLE_TESTS=1 -DCMAKE_INSTALL_PREFIX=$PREFIX -DPYTHON_SITE_PACKAGES_DIR=$SP_DIR -DCMAKE_FIND_ROOT_PATH=$PREFIX
 cmake --build build --target install
-
-ln -sv $PREFIX/lib/libimg_mod.so $PREFIX/lib/libimg_mod.so.1.0
-ln -sv $PREFIX/lib/libimg_mod.so $PREFIX/lib/libimg_mod.so.1.0.0
-ln -sv $PREFIX/lib/libxspress3.so $PREFIX/lib/libxspress3.so.1.0
-ln -sv $PREFIX/lib/libxspress3.so $PREFIX/lib/libxspress3.so.1.0.0
